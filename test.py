afe650f8fa5e904c1d094b717dedd6c18339be61
varshitfauzdar <varshitfauzdar2004@gmail.com>
2025-08-31 21:23:05 +0530

    modular coding

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 from oops import chatbook
 user1 = chatbook()
 print(user1.id)     #using modular coding here instead of importing the class from the oops.py file
+
+
+"""Modular Coding (or Modular Programming)
+
+Modular coding is a software design technique where a program is divided into smaller, independent, reusable parts called modules.
+
+Each module:
+
+Has a specific task or functionality.
+
+Can be developed, tested, and debugged separately.
+
+Can be reused in other projects."""
